43b48e50800a75f26cca0f35b4be752c7566c6a8
Tambosa <tambosa@gmail.com>
2022-06-14 07:06:46 +0300

    рефактор пакетов

---
+++ app/src/main/java/com/gb/stopwatch/ui/view/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.gb.stopwatch.ui.view
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.gb.stopwatch.R
+import com.gb.stopwatch.domain.utils.TimestampMillisecondsFormatter
+import com.gb.stopwatch.domain.repository.ElapsedTimeCalculator
+import com.gb.stopwatch.domain.repository.StopwatchStateCalculator
+import com.gb.stopwatch.ui.viewmodel.StopwatchListOrchestrator
+import com.gb.stopwatch.domain.repository.StopwatchStateHolder
+import com.gb.stopwatch.domain.utils.TimestampProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val timestampProvider = object : TimestampProvider {
+        override fun getMilliseconds(): Long {
+            return System.currentTimeMillis()
+        }
+    }
+    private val stopwatchListOrchestrator = StopwatchListOrchestrator(
+        StopwatchStateHolder(
+            StopwatchStateCalculator(
+                timestampProvider,
+                ElapsedTimeCalculator(timestampProvider)
+            ),
+            ElapsedTimeCalculator(timestampProvider),
+            TimestampMillisecondsFormatter()
+        ),
+        CoroutineScope(
+            Dispatchers.Main
+                    + SupervisorJob()
+        )
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_time)
+        CoroutineScope(
+            Dispatchers.Main
+                    + SupervisorJob()
+        ).launch {
+            stopwatchListOrchestrator.ticker.collect {
+                textView.text = it
+            }
+        }
+
+        findViewById<Button>(R.id.button_start).setOnClickListener {
+            stopwatchListOrchestrator.start()
+        }
+        findViewById<Button>(R.id.button_pause).setOnClickListener {
+            stopwatchListOrchestrator.pause()
+        }
+        findViewById<Button>(R.id.button_stop).setOnClickListener {
+            stopwatchListOrchestrator.stop()
+        }
+    }
+}
+
